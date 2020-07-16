4fa2ed8f804b95fbccb83697be7e808039ad1274
Nyururukelvin <nyururukelvin9@gmail.com>
2020-07-16 18:15:29 +0300

    my commit add a control flow and alert messages

--- js/script.js
+++ js/script.js
@@ -4,4 +4,15 @@ function getDay(){
     var year = document.getElementById('year').value;
     var century = year.slice(0, 1);
     var years = year.slice(2, 3);
+
+    if (year == '' || year.length > 4 ){
+        alert('Enter the year correctly');
+    } else if(month == '' || month < 1 || month > 12) {
+        alert('Enter the month correctly');
+    } else if(date == '' || date < 0 || date > 31) {
+        alert('Enter the date correctly');
+    } else {
+        var day = Math.floor( ( ( (century/4) -2*century-1) + ((5*years/4) ) + ((26*(month+1)/10)) + date) % 7);
+        return day
+    }
 }
