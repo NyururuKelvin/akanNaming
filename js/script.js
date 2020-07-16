01f80fc2625c6e09b3a1ee10b8d1aeb7a96273fa
Nyururukelvin <nyururukelvin9@gmail.com>
2020-07-16 18:40:24 +0300

    my commit add days array

--- js/script.js
+++ js/script.js
@@ -28,4 +28,5 @@ function getGender() {
 function getName() {
     var male = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
     var female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+    var day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 }
