44f9fbcb0bebe742dad8157d602db5e78b100690
Nyururukelvin <nyururukelvin9@gmail.com>
2020-07-16 18:05:05 +0300

    my commit add a function to get the day

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function getDay(){
+    var date = parseInt(document.getElementById('date').value);
+    var month = parseInt(document.getElementById('month').value);
+    var year = document.getElementById('year').value;
+    var century = year.slice(0, 1);
+    var years = year.slice(2, 3);
+}
