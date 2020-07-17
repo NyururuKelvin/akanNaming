0ccf1452daf760b1d61f7403ad6ab9408a27c579
Nyururukelvin <nyururukelvin9@gmail.com>
2020-07-17 12:16:51 +0300

    my commit add control flow to getName function

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function getDay(){
 function getGender() {
     var input = document.getElementById('gender');
     for (var i = 0; i < input.length; i++) {
-        if (input[i]).checked) {
+        if (input[i].checked) {
             var gender = input[i].value;
             return gender;
         }
@@ -29,4 +29,11 @@ function getName() {
     var male = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
     var female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
     var day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    if (getGender() == 'male' && getDay() >= 0) {
+        document.getElementById('post').innerHTML = ('You are a male born in Ghana, your Akan name is ') + male[getDay(name)] + (' and born on ') + day[getDay(name)];
+    } else if (getGender() == 'female' && getDay() >= 0) {
+        document.getElementById('post').innerHTML = ('You are a female born in Ghana, your Akan name is ') + female[getDay(name)] + ('and born on ') + day[getDay(name)]; 
+    } else {
+        alert('Kindly Check your details and enter them correctly then retry');
+    }
 }
