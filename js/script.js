2245f072478d233833c9f1e70e83124f736b7db7
Nyururukelvin <nyururukelvin9@gmail.com>
2020-07-18 10:57:09 +0300

    my commit edit functions

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-function getDay(){
-    var date = parseInt(document.getElementById('date').value);
+function getDay(name){
+    var date = parseInt(document.getElementById('day').value);
     var month = parseInt(document.getElementById('month').value);
     var year = document.getElementById('year').value;
     var century = year.slice(0, 1);
     var years = year.slice(2, 3);
 
-    if (year == '' || year.length > 4 ){
+    if (year === '' || year.length > 4 ){
         alert('Enter the year correctly');
-    } else if(month == '' || month < 1 || month > 12) {
+    } else if(month === '' || month < 1 || month > 12) {
         alert('Enter the month correctly');
-    } else if(date == '' || date < 0 || date > 31) {
+    } else if(date === '' || date < 0 || date > 31) {
         alert('Enter the date correctly');
     } else {
         var day = Math.floor( ( ( (century/4) -2*century-1) + ((5*years/4) ) + ((26*(month+1)/10)) + date) % 7);
@@ -17,27 +17,24 @@ function getDay(){
     }
 }
 function getGender() {
-    var input = document.getElementById('gender');
-    for (var i = 0; i < input.length; i++) {
-        if (input[i].checked) {
-            var gender = input[i].value;
+    var radio = document.getElementById('gender');
+    for (var i = 0; i < radio.length; i++) {
+        if (radio[i].checked) {
+            var gender = radio[i].value;
             return gender;
         }
     }
 }
 function getName() {
+    var name = document.getElementById('name').value;
     var male = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
     var female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
     var day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     if (getGender() == 'male' && getDay() >= 0) {
-        document.getElementById('post').innerHTML = ('You are a male born in Ghana, your Akan name is ') + male[getDay(name)] + (' and born on ') + day[getDay(name)];
+        document.getElementById('post').innerHTML = ('Hey ') + (document.getElementById('name').value) + (' You are a male born in Ghana, your Akan name is ') + male[getDay(name)] + (' and born on ') + day[getDay(name)];
     } else if (getGender() == 'female' && getDay() >= 0) {
-        document.getElementById('post').innerHTML = ('You are a female born in Ghana, your Akan name is ') + female[getDay(name)] + ('and born on ') + day[getDay(name)]; 
+        document.getElementById('post').innerHTML = ('Hey ') + (document.getElementById('name').value) + (' You are a female born in Ghana, your Akan name is ') + female[getDay(name)] + ('and born on ') + day[getDay(name)]; 
     } else {
         alert('Kindly Check your details and enter them correctly then retry');
     }
-}
-function result() {
-    var showResult = document.getElementById('form');
-    showResult.style.display = 'block';
 }
